45c9ad523c4fe49e3a45a9bb27990c179d940553
Eric Simard <ericsimard52@gmail.com>
2018-12-26 19:46:26 -0600

    update

--- src/pocketmine/command/defaults/PluginsCommand.php
+++ src/pocketmine/command/defaults/PluginsCommand.php
@@ -55,7 +55,7 @@ class PluginsCommand extends VanillaCommand{
 
     }
     if ($args[0] === "load"){
-      $sender->getServer()->getPluginManager()->loagPlugin("plugins/PureEntitiesX/src/revivalpmmp/pureentities/PureEntities.php");
+      $sender->getServer()->getPluginManager()->loagPlugin("plugins/PureEntitiesX/src/revivalpmmp/pureentities");
     }
   }
 }
